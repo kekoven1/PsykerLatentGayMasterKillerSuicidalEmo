828e86ad67713f2a93bbf99cf3f6771296d6c517
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-12-12 18:05:58 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/Administration/Notifications/ClientAdminNotificationSystem.cs
+++ Content.Goobstation.Client/Administration/Notifications/ClientAdminNotificationSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Goob Station Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Common.Administration.Notifications;
 using Content.Goobstation.Common.CCVar;
 using Content.Goobstation.Shared.Administration.Notifications;
--- Content.Goobstation.Shared/Changeling/Systems/SharedVoidAdaptionSystem.cs
+++ Content.Goobstation.Shared/Changeling/Systems/SharedVoidAdaptionSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Goob Station Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Common.Atmos;
 using Content.Goobstation.Common.Temperature;
 using Content.Goobstation.Shared.Atmos.Events;
